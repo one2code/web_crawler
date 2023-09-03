8c87bbc06242019dbc49620f19ef578a1d83ccec
Travis Bennett <t_bennett_a@protonmail.com>
2023-09-02 23:17:24 -0400

    Added http test case

--- crawl.test.ts
+++ crawl.test.ts
@@ -7,3 +7,7 @@ test('normalizeUrl should remove trailing slash', () => {
     expect(normalizeUrl(url)).toBe('https://www.example.com');
 });
 
+test('normalizeUrl should return the same url for https and http', () => {
+    const http = 'http://www.example.com';
+    expect(normalizeUrl(http)).toBe('https://www.example.com');
+})
--- crawl.ts
+++ crawl.ts
@@ -1,4 +1,6 @@
 export function normalizeUrl(url: string): string{
-
+  if (url.startsWith('http://')) {
+    url = url.replace('http://', 'https://');
+  }
+return url.replace(/\/$/, '')
 }
-
