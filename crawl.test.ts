21a24e92a38b44a2ffb34d4e75d88c8e415d325a
Travis Bennett <t_bennett_a@protonmail.com>
2023-09-02 23:41:02 -0400

    more test cases

--- crawl.test.ts
+++ crawl.test.ts
@@ -9,6 +9,25 @@ test("convert http to https", () => {
 	expect(normalizeUrl("http://example.com")).toBe("https://example.com");
 });
 
-test("handles mixed cases", () => {
-	expect(normalizeUrl("http://eXaMPle.com")).toBe("http://example.com");
+test("handle mixed cases", () => {
+	expect(normalizeUrl("https://eXaMPle.com")).toBe("https://example.com");
 });
+
+test("handle query parameters", () => {
+	expect(normalizeUrl("https://example.com/?key=value")).toBe(
+		"https://example.com/?key=value"
+	);
+});
+
+test("handle no protocol", () => {
+	expect(normalizeUrl("example.com")).toBe("https://example.com");
+});
+
+test('handle URL fragments', () => {
+    expect(normalizeUrl("https://example.com/#section")).toBe("https://example.com/#section");
+})
+
+test('throw error for invalid URLs', () => {
+    expect(normalizeUrl("not a url")).toThrow("Invalid URL");
+})
+
--- crawl.ts
+++ crawl.ts
@@ -2,5 +2,12 @@ export function normalizeUrl(url: string): string{
   if (url.startsWith('http://')) {
     url = url.replace('http://', 'https://');
   }
+  
+  try {
+    new URL(url);
+  } catch (e) {
+    throw new Error(`Invalid URL ${url}`);
+  }
+
 return url.replace(/\/$/, '')
 }
